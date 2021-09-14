ec32ed2f46cf2b0fc4490c278a5e1083d838a1fd
dr0p <ldeni@softserveinc.com>
2021-09-14 15:29:55 +0300

    added some context to the locking example

--- locking.js
+++ locking.js
@@ -1,25 +1,20 @@
 /*
-This is a very crude torture example in multi threading with the modified GraalJS. This example should not crash, BUT will
-experience concurrency issues - the counter will almost never be correct and this is expected
+This example shows how to deadlock the GraalJS by having unsynchronized writes to object properties.
 
+Run this example several times - at some point the example will get stuck at printing `{someNumber} threads are still active.`
+and you will notice that the CPU usage of the example is at (100 * someNumber)%
 */
 
-const threadCount = 16;
+const threadCount = 12;
 const Runnable = Java.type('java.lang.Runnable');
 const Thread = Java.type('java.lang.Thread');
 const ExtendableRunnable = Java.extend(Runnable);
 const testObject = {};
-const testArray = [];
-
-const rwl = new (Java.type('java.util.concurrent.locks.ReentrantReadWriteLock'))(true);
-const lock_r = rwl.readLock();
-const lock_w = rwl.writeLock();
 
 const _runnable = new ExtendableRunnable({
   run: function () {
     for (let i = 0; i < 1000; i++) {
       testObject[Math.random().toString()] = new Object();
-      testArray.push(new Object());
     }
   }
 });
@@ -54,4 +49,3 @@ threads.forEach(t => t.join());
 threadCounter.join();
 
 print(`testObject has ${Object.keys(testObject).length} keys.`)
-print(`testArray has ${testArray.length} values.`)
